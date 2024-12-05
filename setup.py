eba8d6170a4f6080a6344c800b29125df4d3863f
David Gomes <david.ef.gomes@ubi.pt>
2024-12-05 00:15:54 +0000

    1st 2nd commit

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
-from distutils.core import setup
-setup(name='drawing',
-      version='1.0',
-      description='Drawing Distribution Package',
-      author='Your Name',
-      author_email='Your Email',
-      url='The URL project - github for example',
-      packages=['drawing'],
-     )
+from codecs import open
+from os.path import join, abspath, dirname
+from setuptools import setup, find_packages
+import os
+
+requirementPath = 'requirements.txt'
+install_requires = []
+
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+here = abspath(dirname(__file__))
+
+# Get the long description from the README file
+with open(join(here, 'README.md'), encoding='utf-8') as buff:
+    long_description = buff.read()
+
+setup(
+    name="drawing",
+    version="0.1",
+    description="Drawing Distribution Package",
+    long_description=long_description,
+    license='MIT',
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    install_requires=install_requires
+)
